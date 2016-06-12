836094ea412bd6ba58d70e42ae338dcfd142c108
Aaron <aarongkendall@gmail.com>
2016-06-12 12:52:01 +0100

    main.js handling loading of page components

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 import Hello from './src/hello.jsx';
 import World from './src/world.jsx';
+
+ReactDOM.render(
+  <div>
+    <Hello />
+    <World />
+  </div>,
+  document.getElementById('container')
+);
