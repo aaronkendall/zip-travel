00e2fad50842791b591bef32f3a260d15f8a5281
Aaron <aarongkendall@gmail.com>
2016-06-12 12:54:27 +0100

    file structure altered

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Hello from './src/hello.jsx';
-import World from './src/world.jsx';
+import Hello from './components/hello.jsx';
+import World from './components/world.jsx';
 
 ReactDOM.render(
   <div>
