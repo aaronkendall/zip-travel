3bf5b7b5c157a868a0d6fbed4e6ce4beee8ebd3e
Aaron <aarongkendall@gmail.com>
2016-06-15 22:48:22 +0100

    FlightsList component started, formatting of returned data next

---
+++ src/components/FlightsList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class FlightsList extends React.Component {
+
+  render() {
+    return(
+      <div>
+        // return a formatted list of some kind
+        this.props.data
+      </div>
+    )
+  };
+};
+
+export default FlightsList;
--- src/components/TravelSearch.jsx
+++ src/components/TravelSearch.jsx
@@ -3,6 +3,7 @@ import reqwest from 'reqwest';
 
 import SubmitButton from './SubmitButton.jsx';
 import SearchBox from './SearchBox.jsx';
+import FlightsList from './FlightsList.jsx';
 
 class TravelSearch extends React.Component {
   constructor(props, context) {
@@ -10,6 +11,7 @@ class TravelSearch extends React.Component {
     this.handleChange = this.handleChange.bind(this);
     this.toggleLoading = this.toggleLoading.bind(this);
     this.getFlightData = this.getFlightData.bind(this);
+    this.createFlightList = this.createFlightList.bind(this);
     this.state = {
       value: '',
       results: [],
@@ -32,20 +34,29 @@ class TravelSearch extends React.Component {
       url: 'http://localhost:3000/flights',
       type: 'json',
       method: 'post',
-      data: { 'origin': this.state.value, 'startDate': '2016-06-20'}
+      data: {'origin': this.state.value, 'startDate': '2016-06-20', 'endDate': 'a thing'}
     }).then(function(response) {
-      self.state.results = response.body;
+      self.state.results.push(response);
       self.toggleLoading();
     });
   }
 
+  createFlightList() {
+    if (this.state.results) {
+      return <FlightsList data={this.state.results} />
+    }
+    return null
+  };
+
   render() {
     const loadingSpinner = this.state.loading ? <img src={'../src/img/loading.gif'} /> : null;
+    const flightResults = this.createFlightList();
     return(
       <div>
         {loadingSpinner}
         <SearchBox value={this.state.value} handleChange={this.handleChange} />
         <SubmitButton handleClick={this.getFlightData} />
+        {flightResults}
       </div>
     );
   }
